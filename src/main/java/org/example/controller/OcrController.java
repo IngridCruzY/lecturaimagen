7b2620f21779083492e35767c6284ce27f875380
IngridCruz22 <ingrid2209c@gmail.com>
2024-08-05 18:34:50 -0500

    fix

--- src/main/java/org/example/controller/OcrController.java
+++ src/main/java/org/example/controller/OcrController.java
@@ -13,16 +13,29 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 @RestController
+@RequestMapping("/api/extract")
 public class OcrController {
     @Autowired
     private OcrService ocrService;
 
+    /*
     @PostMapping("/image-text")
     public String extractTextFromImage(@RequestParam("file") MultipartFile file) {
         try {
             return ocrService.extractTextFromImage(file);
         } catch (IOException | TesseractException | URISyntaxException e) {
             return "Error al procesar la imagen: " + e.getMessage();
+        }
+    }
+
+     */
+
+    @PostMapping("/image2-text")
+    public String extractTextFromImage2(@RequestParam("file") MultipartFile file) {
+        try {
+            return ocrService.extractTextFromImage2(file);
+        } catch (IOException | TesseractException | URISyntaxException e) {
+            return "Error al procesar la imagen de Yape: " + e.getMessage();
         }
     }
 }
--- src/main/java/org/example/service/OcrService.java
+++ src/main/java/org/example/service/OcrService.java
@@ -2,6 +2,9 @@ package org.example.service;
 
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,20 +16,56 @@ import java.nio.file.Paths;
 @Service
 public class OcrService {
 
+    static {
+        try {
+            // Cargar la biblioteca nativa desde el JAR (dentro del archivo .jar)
+            String path = OcrService.class.getResource("/native/opencv_java460.dll").getPath();
+            System.load(path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+/*
     public String extractTextFromImage(MultipartFile file) throws IOException, TesseractException, URISyntaxException {
 
         File tempFile = File.createTempFile("uploaded_", file.getOriginalFilename());
         file.transferTo(tempFile);
 
         Tesseract tesseract = new Tesseract();
-        //tesseract.setDatapath("C:/Program Files/Tesseract-OCR/tessdata");
         File tessDataFolder = Paths.get(getClass().getClassLoader().getResource("tessdata").toURI()).toFile();
         tesseract.setDatapath(tessDataFolder.getAbsolutePath());
 
         String result = tesseract.doOCR(tempFile);
-
         tempFile.delete();
 
         return result;
     }
+
+ */
+
+    public String extractTextFromImage2(MultipartFile file) throws IOException, TesseractException, URISyntaxException {
+
+        File tempFile = File.createTempFile("uploaded_", file.getOriginalFilename());
+        file.transferTo(tempFile);
+
+        Mat src = Imgcodecs.imread(tempFile.getAbsolutePath());
+        Mat gray = new Mat();
+        Imgproc.cvtColor(src, gray, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.adaptiveThreshold(gray, gray, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 15, 5);
+
+        Imgcodecs.imwrite(tempFile.getAbsolutePath(), gray);
+
+        Tesseract tesseract = new Tesseract();
+        File tessDataFolder = Paths.get(getClass().getClassLoader().getResource("tessdata").toURI()).toFile();
+        tesseract.setDatapath(tessDataFolder.getAbsolutePath());
+        tesseract.setLanguage("spa");
+        tesseract.setPageSegMode(3);
+
+        String result = tesseract.doOCR(tempFile);
+        tempFile.delete();
+
+        return result;
+
+    }
+
 }
